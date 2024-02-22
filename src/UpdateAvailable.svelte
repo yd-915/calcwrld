84655d059ba864818a6835fef3b2222bb3208506
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2024-02-21 22:35:23 -0500

    Update UpdateAvailable.svelte

--- src/UpdateAvailable.svelte
+++ src/UpdateAvailable.svelte
@@ -1,6 +1,6 @@
 <p>
-  An update is available for EngineeringPaper.xyz. The next time you close all 
-  of your windows and tabs and reopen EngineeringPaper.xyz, you'll receive the update
+  An update is available for CalcWrld. The next time you close all 
+  of your windows and tabs and reopen CalcWrld, you'll receive the update
   (note that refreshing the page is not sufficient to trigger the update).
   You will know that the update has been applied when this icon disappears. It 
   is safe to continue working until you are ready to update.
